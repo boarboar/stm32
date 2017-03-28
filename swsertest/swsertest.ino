b2e31189eb2085b74c8241fd4b3dd415ac97c69f
Alex Ershov <alex.ershov@gmail.com>
2017-03-28 16:45:32 +0300

    crc

--- swsertest/swsertest.ino
+++ swsertest/swsertest.ino
@@ -3,6 +3,27 @@
 
 SoftwareSerial swSer(12, 13, false, 256);
 
+uint8_t docrc(char *buf) {
+  // check crc
+  uint8_t crc=0, i;
+  uint8_t pos=0;
+  
+  while(buf[pos])
+    {      
+      crc = crc ^ buf[pos];
+      for (i=0; i<8; i++) {
+        if (crc & 1) {
+            crc = (crc >> 1) ^0x8c;
+        }
+        else {
+            crc = (crc >> 1);
+        }
+      }
+    }
+
+ return crc;
+}   
+    
 void setup() {
   delay(2000);
   Serial.begin(115200);
@@ -28,12 +49,23 @@ void loop() {
   }
 */
 delay(100);
-swSer.println("G 1");
+char buf[20];
+strcpy(buf, "G 1");
+swSer.print(buf);
+swSer.print("#");
+swSer.println(docrc(buf));
+
 while (swSer.available() > 0) {
     Serial.write(swSer.read());
   }
+  
+delay(100);
 
-swSer.println("S 1,[2,34]");
+strcpy(buf, "S 1,[2,34]");
+swSer.print(buf);
+swSer.print("#");
+swSer.println(docrc(buf));
+
 while (swSer.available() > 0) {
     Serial.write(swSer.read());
   }
